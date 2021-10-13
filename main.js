0d798bde67965d3dee4c12fe973a125601f6feab
ivnikonGB <ivnikon@yandex.ru>
2021-10-13 22:56:31 +0300

    Task #3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const subzero = {
     name: 'Subzero',
     hp: 100,
-    img: 'image1.jpg',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: [
         'weapon1',
         'weapon2',
@@ -15,7 +15,7 @@ const subzero = {
 const liuKang = {
     name: 'Liu Kang',
     hp: 80,
-    img: 'image2.jpg',
+    img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: [
         'weapon5',
         'weapon6',
@@ -26,7 +26,7 @@ const liuKang = {
     }
 }
 
-function createPlayer(player, name, life) {
+function createPlayer(player, playerObject) {
     const $root = document.querySelector('.arenas');
     const $player = document.createElement('div');
     const $progressBar = document.createElement('div');
@@ -47,16 +47,16 @@ function createPlayer(player, name, life) {
     $progressBar.appendChild($name);
     $character.appendChild($img);
 
-    $name.innerText = name;
-    $life.innerText = life;
-    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+    $name.innerText = playerObject.name;
+    $life.innerText = playerObject.hp;
+    $img.src = playerObject.img;
 
     $life.style.width = '100%';
 
     $root.appendChild($player);
 }
 
-createPlayer('player1', 'Subzero', 100);
-createPlayer('player2', 'Liu Kang', 80);
+createPlayer('player1', subzero);
+createPlayer('player2', liuKang);
 
 
