d318842ee00bcd9fdc24e2c12276f510bf86d6e7
ivnikonGB <ivnikon@yandex.ru>
2021-10-13 22:47:11 +0300

    Task #1

--- main.js
+++ main.js
@@ -26,4 +26,33 @@ const liuKang = {
     }
 }
 
+function createPlayer() {
+    const $root = document.querySelector('.arenas');
+    const $player = document.createElement('div');
+    const $progressBar = document.createElement('div');
+    const $life = document.createElement('div');
+    const $name = document.createElement('div');
+    const $character = document.createElement('div');
+    const $img = document.createElement('img');
+
+    $player.classList.add('player1');
+    $progressBar.classList.add('progressBar');
+    $life.classList.add('life');
+    $name.classList.add('name');
+    $character.classList.add('character');
+
+    $player.appendChild($progressBar);
+    $player.appendChild($character);
+    $progressBar.appendChild($life);
+    $progressBar.appendChild($name);
+    $character.appendChild($img);
+
+    $name.innerText = 'Liu Kang';
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+
+    $life.style.width = '100%';
+
+    $root.appendChild($player);
+}
+
 
