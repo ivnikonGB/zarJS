de6ab31733babbe97535f06633db88fa6aa0d60a
ivnikonGB <ivnikon@yandex.ru>
2021-10-13 22:26:11 +0300

    task #0

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert('Fight...');
+const subzero = {
+    name: 'Subzero',
+    hp: 100,
+    img: 'image1.jpg',
+    weapon: [
+        'weapon1',
+        'weapon2',
+        'weapon3',
+    ],
+    attack: function() {
+        console.log(this.name + ' Fight...');
+    }
+}
+
+const liuKang = {
+    name: 'Liu Kang',
+    hp: 80,
+    img: 'image2.jpg',
+    weapon: [
+        'weapon5',
+        'weapon6',
+        'weapon7',
+    ],
+    attack: function() {
+        console.log(this.name + ' Fight...');
+    }
+}
+
+
